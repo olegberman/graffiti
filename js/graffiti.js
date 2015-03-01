7d0da3a8183bbab321154e0edd18bee73e431452
Oleg Berman <bermanoleg@gmail.com>
2015-03-02 00:59:04 +0300

    drawing, refactoring and buggy history

--- js/graffiti.js
+++ js/graffiti.js
@@ -2,14 +2,20 @@ var graffiti = {
 
   brush: [20, "95, 127, 162", 1], // size, rgb color, opacity
   strokes: [],
+  drawAreaMaxWidth: 1240,
+  drawAreaMaxHeight: 620,
+  historyLimit: 50,
 
   brushPreviewCanvas: undefined,
   brushPreviewContext: undefined,
+  colorPreviewBox: undefined,
   colorPickerWrap: undefined,
   colorPicker: undefined,
   colorPickerActiveCell: undefined,
-  drawAreaCanvas: undefined,
-  drawAreaContext: undefined,
+  drawAreaMainCanvas: undefined,
+  drawAreaMainContext: undefined,
+  drawAreaStrokeCanvas: undefined,
+  drawAreaStrokeContext: undefined,
   drawAreaWrap: undefined,
 
   init: function() {
@@ -29,9 +35,9 @@ var graffiti = {
 
   attachEvents: function() {
     document.addEventListener("mousemove", function() {
-      //console.log(event);
+      event.preventDefault();
       graffiti.sliderMouseMove(event);
-      graffiti.drawAreaEventPool(event);
+      graffiti.drawAreaAdvanceStroke(event);
     }, false);
     document.addEventListener("mouseup", function() {
       graffiti.sliderMouseUp();
@@ -39,23 +45,12 @@ var graffiti = {
     }, false);
   },
 
-  normalizeCanvasRatios: function(canvas, context) {
-    var w = canvas.width;
-    var h = canvas.height;
-    var ratio = window.devicePixelRatio;
-    canvas.width *= ratio;
-    canvas.height *= ratio;
-    canvas.style.width = w + "px";
-    canvas.style.height = h + "px";
-    context.scale(ratio, ratio);
-  },
-
   // brush preview
 
   brushPreviewInit: function() {
     graffiti.brushPreviewCanvas = ge("graffiti_brush_canvas");
     graffiti.brushPreviewContext = graffiti.brushPreviewCanvas.getContext("2d");
-    graffiti.normalizeCanvasRatios(graffiti.brushPreviewCanvas, graffiti.brushPreviewContext);
+    graffiti.brushPreviewContext.scale(2, 2);
     graffiti.brushPreviewUpdate();
   },
 
@@ -66,7 +61,6 @@ var graffiti = {
     ctx.lineJoin = "round";
     ctx.lineWidth = graffiti.brush[0];
     ctx.strokeStyle = "rgba(" + graffiti.brush[1] + ", " + graffiti.brush[2] + ")";
-    console.log("rgba(" + graffiti.brush[1] + ", " + graffiti.brush[2] + ")");
     ctx.beginPath();
     ctx.moveTo(40, 40);
     ctx.lineTo(40, 40.5);
@@ -81,6 +75,7 @@ var graffiti = {
     graffiti.colorPickerWrap = ge("graffiti_colorpicker_wrap");
     graffiti.colorPicker = ge("graffiti_colorpicker");
     graffiti.colorPickerActiveCell = ge("graffiti_colorpicker_cell_active");
+    graffiti.colorPreviewBox = ge("graffiti_colorpreview_box");
     var position = getXY(ge("graffiti_colorpreview_wrap"));
     graffiti.colorPickerWrap.style.left = position[0] - 50 + "px";
     graffiti.colorPickerWrap.style.top = position[1] + 30 + "px";
@@ -89,7 +84,8 @@ var graffiti = {
     for(var r = 0; r < 6; r++) {
       for(var g = 0; g < 6; g++) {
         for(var b = 0; b < 6; b++) {
-          colors[r * 36 + g * 6 + b] = "rgb(" + (r / 5 * 255) + "," + (g / 5 * 255) + "," + (b / 5 * 255) + ")";
+          colors[r * 36 + g * 6 + b] = "rgb(" + (r / 5 * 255) + "," 
+          + (g / 5 * 255) + "," + (b / 5 * 255) + ")";
         }
       }
     }
@@ -116,7 +112,9 @@ var graffiti = {
     if(graffiti.colorPickerOpened == 0) {
       var position = getXY(ge("graffiti_colorpreview_wrap"));
       graffiti.colorPickerWrap.style.display = "block";
-      animate(graffiti.colorPickerWrap, { opacity: 1, top: position[1] - 230 }, 100);
+      animate(graffiti.colorPickerWrap, { opacity: 1, 
+                                          top: position[1] - 230 },
+                                          100);
       graffiti.colorPickerOpened = 1;
     } else {
       graffiti.colorPickerHide();
@@ -125,7 +123,11 @@ var graffiti = {
 
   colorPickerHide: function() {
     var position = getXY(ge("graffiti_colorpreview_wrap"));
-    animate(graffiti.colorPickerWrap, { opacity: 0, top: position[1] + 30 }, 100, function() {
+    graffiti.colorPickerActiveCell.style.display = "none";
+    animate(graffiti.colorPickerWrap, { opacity: 0, 
+                                        top: position[1] + 30 }, 
+                                        100,
+    function() {
       graffiti.colorPickerWrap.style.display = "none";
     });
     graffiti.colorPickerOpened = 0;
@@ -133,7 +135,8 @@ var graffiti = {
 
   colorPickerHighlight: function(target) {
     if(graffiti.colorPickerOpened == 1) {
-      graffiti.colorPickerLastHighlight = target.style.backgroundColor.replace(/(rgb\(|\))/g, "");
+      var cleanRGB = target.style.backgroundColor.replace(/(rgb\(|\))/g, "")
+      graffiti.colorPickerLastHighlight = cleanRGB;
       var position = getXY(target);
       graffiti.colorPickerActiveCell.style.display = "block";
       graffiti.colorPickerActiveCell.style.left = position[0] + "px";
@@ -145,7 +148,9 @@ var graffiti = {
 
   colorPickerChooseColor: function() {
     graffiti.brush[1] = graffiti.colorPickerLastHighlight;
+    graffiti.colorPreviewBox.style.backgroundColor = "rgb(" + graffiti.brush[1] + ")";
     graffiti.brushPreviewUpdate();
+    graffiti.colorPickerHide();
   },
 
   // sliders
@@ -191,13 +196,18 @@ var graffiti = {
   sliderMove: function(event) {
     event.preventDefault();
     var pixelPosition;
-    var slider = isEmpty(graffiti.sliderActive) ? graffiti.sliderHovered : graffiti.sliderActive;
+    var slider = undefined;
+    if(isEmpty(graffiti.sliderActive)) {
+      slider = graffiti.sliderHovered;
+    } else {
+      slider = graffiti.sliderActive;
+    }
     var width = getSize(slider.wrapper)[0] - 5;
     if(event.type == "wheel" || event.type == "DOMMouseScroll") {
       var delta = (event.detail < 0 || event.wheelDelta > 0) ? -5 : 5;
       pixelPosition = (width / 100 * slider.value) + delta;
     } else {
-      pixelPosition = event.clientX - getXY(slider.wrapper)[0];
+      pixelPosition = event.pageX - getXY(slider.wrapper)[0];
     }
     slider.value = pixelPosition / width * 100;
     if(pixelPosition > width) pixelPosition = width, slider.value = 100;
@@ -229,53 +239,171 @@ var graffiti = {
 
   drawAreaInit: function() {
     graffiti.drawAreaWrap = ge("graffiti_drawarea_wrap");
-    graffiti.drawAreaCanvas = ge("graffiti_drawarea_canvas");
-    graffiti.drawAreaContext = graffiti.drawAreaCanvas.getContext("2d");
+    graffiti.drawAreaMainCanvas = ge("graffiti_canvas_main");
+    graffiti.drawAreaMainContext = graffiti.drawAreaMainCanvas.getContext("2d");
+    graffiti.drawAreaStrokeCanvas = ge("graffiti_canvas_stroke");
+    graffiti.drawAreaStrokeContext = graffiti.drawAreaStrokeCanvas.getContext("2d");
+    graffiti.drawAreaMainContext.scale(2, 2);
+    graffiti.drawAreaStrokeContext.scale(2, 2);
     graffiti.drawAreaWrapOffset = getXY(graffiti.drawAreaWrap);
   },
 
-  drawAreaEventPool: function(event) {
-    if(graffiti.drawAreaInUse == 1) {
-      switch(event.type) {
-        case "mousemove":
-          var xy = getXY(graffiti.drawAreaWrap);
-          graffiti.strokes.push([event.clientX - xy[0], event.clientY - xy[1]]);
-          console.log([event.clientX - xy[0], event.clientY - xy[1]]);
-          graffiti.drawAreaDraw();
-        break;
-      }
-    }
-  },
-
-  drawAreaDraw: function() {
-    var ctx = graffiti.drawAreaContext;
-    ctx.lineCap = "round";
-    ctx.lineJoin = "round";
-    ctx.lineWidth = graffiti.brush[0];
-    ctx.strokeStyle = "rgba(" + graffiti.brush[1] + ", " + graffiti.brush[2] + ")";
-    ctx.beginPath();
-    console.log(graffiti.strokes);
-    if(graffiti.strokes.length > 2) {
-      var st1 = graffiti.strokes.shift();
-      ctx.moveTo(st1[0], st1[1]);
-      var st2 = graffiti.strokes.shift();
-      ctx.lineTo(st2[0], st2[1]);
-      ctx.closePath();
-      ctx.stroke();
-    }
-  },
-
   drawAreaBeginStroke: function(event) {
+    var pos = graffiti.drawAreaWrapOffset;
+    graffiti.strokes.push([event.pageX - pos[0], event.pageY - pos[1]]);
     graffiti.drawAreaInUse = 1;
-    graffiti.strokes.push([]);
+    graffiti.draw(graffiti.drawAreaStrokeContext);
+  },
+
+  drawAreaAdvanceStroke: function(event) {
+    if(graffiti.drawAreaInUse == 1) {
+      var maxW = graffiti.drawAreaMaxWidth;
+      var maxH = graffiti.drawAreaMaxHeight;
+      graffiti.drawAreaStrokeContext.clearRect(0, 0, maxW, maxH);
+      var pos = graffiti.drawAreaWrapOffset;
+      graffiti.strokes.push([event.pageX - pos[0], event.pageY - pos[1]]);
+      graffiti.draw(graffiti.drawAreaStrokeContext);
+    }
   },
 
   drawAreaFinishStroke: function() {
     if(graffiti.drawAreaInUse == 1) {
+      var data = graffiti.canvasCloneGetData();
       graffiti.drawAreaInUse = 0;
+      graffiti.draw(graffiti.drawAreaMainContext);
+      graffiti.drawAreaStrokeContext.clearRect(0, 0, data[2], data[3]);
+      graffiti.historyAddStep(graffiti.strokes, graffiti.brush);
+      graffiti.strokes = [];
     }
-  }
+  },
 
+  drawAreaErase: function() {
+    var data = graffiti.canvasCloneGetData();
+    graffiti.drawAreaStrokeContext.drawImage(graffiti.drawAreaMainCanvas, 0, 0, data[0], data[1]);
+    graffiti.drawAreaMainContext.clearRect(0, 0, data[2], data[3]);
+    animate(graffiti.drawAreaStrokeCanvas, {opacity: 0}, 200, function() {
+      graffiti.drawAreaStrokeContext.clearRect(0, 0, data[2], data[3]);
+      graffiti.drawAreaStrokeCanvas.style.opacity = "1";
+    });
+    // history flush here
+  },
+
+  // draw
+
+  draw: function(ctx, history) {
+    var strokes = undefined;
+    var recursive = false;
+    if(history) {
+      if(history.length != 0) {
+        var step = history.pop();
+        strokes = step[0];
+        recursive = true;
+      } else {
+        return false;
+      }
+      ctx.lineWidth = step[1][0];
+      ctx.strokeStyle = "rgba(" + step[1][1] + ", " + step[1][2] + ")";
+    } else {
+      strokes = graffiti.strokes;
+      ctx.lineWidth = graffiti.brush[0];
+      ctx.strokeStyle = "rgba(" + graffiti.brush[1] + ", " + graffiti.brush[2] + ")";
+    }
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.beginPath();
+    if(strokes.length < 2) {
+      ctx.moveTo(strokes[0][0], strokes[0][1]);
+      ctx.lineTo(strokes[0][0] + 0.51, strokes[0][1]);
+    } else {
+      ctx.moveTo(strokes[0][0], strokes[0][1]);
+      ctx.lineTo((strokes[0][0] + strokes[1][0]) * 0.5, 
+                 (strokes[0][1] + strokes[1][1]) * 0.5);
+      var i = 0;
+      while(++i < (strokes.length - 1)) {
+        var abs1 = Math.abs(strokes[i - 1][0] - strokes[i][0]) 
+                 + Math.abs(strokes[i - 1][1] - strokes[i][1])
+                 + Math.abs(strokes[i][0] - strokes[i + 1][0]) 
+                 + Math.abs(strokes[i][1] - strokes[i + 1][1]);
+        var abs2 = Math.abs(strokes[i - 1][0] - strokes[i + 1][0])
+                 + Math.abs(strokes[i - 1][1] -  strokes[i + 1][1]);
+        if(abs1 > 10 && abs2 > abs1 * 0.8) {
+          ctx.quadraticCurveTo(strokes[i][0], strokes[i][1],
+                              (strokes[i][0] + strokes[i + 1][0]) * 0.5,
+                              (strokes[i][1] + strokes[i + 1][1]) * 0.5);
+          continue;
+        }
+      ctx.lineTo(strokes[i][0], strokes[i][1]);
+      ctx.lineTo((strokes[i][0] + strokes[i + 1][0]) * 0.5, 
+                 (strokes[i][1] + strokes[i + 1][1]) * 0.5);
+      }
+      ctx.lineTo(strokes[strokes.length - 1][0], strokes[strokes.length - 1][1]);
+      ctx.moveTo(strokes[strokes.length - 1][0], strokes[strokes.length - 1][1]);
+    }
+    ctx.stroke();
+    ctx.closePath();
+    if(recursive) graffiti.draw(ctx, history);
+  },
+
+  // history
+
+  history: [],
+  historyCheckpoint: undefined,
+
+  historyAddStep: function(strokes, brush) {
+    graffiti.history.push([strokes, brush]);
+    if(graffiti.history.length == graffiti.historyLimit) {
+      graffiti.historyCheckpoint = graffiti.drawAreaMainCanvas.toDataURL("image/png", 1);
+    }
+    if(graffiti.history.length == graffiti.historyLimit * 2) {
+      if(graffiti.historyCheckpoint) {
+        var image = new Image();
+        image.onload = function() {
+          var data = graffiti.canvasCloneGetData();
+          graffiti.drawAreaMainContext.clearRect(0, 0, data[2], data[3]);
+          graffiti.drawAreaMainContext.drawImage(image, 0, 0, data[0], data[1]);
+          graffiti.history.splice(0, graffiti.historyLimit);
+          graffiti.draw(graffiti.drawAreaMainContext, graffiti.history);
+        }
+        image.src = graffiti.historyCheckpoint;
+      }
+    }
+  },
+
+  historyStepBack: function() {
+    var data = graffiti.canvasCloneGetData();
+    if(history.length == 0) {
+      if(!graffiti.historyCheckpoint) return false;
+      var image = new Image();
+      image.onload = function() {
+        graffiti.drawAreaStrokeContext.drawImage(graffiti.drawAreaMainCanvas, 0, 0, data[0], data[1]);
+        graffiti.drawAreaMainContext.drawImage(image, 0, 0, data[0], data[1]);
+        animate(graffiti.drawAreaStrokeCanvas, { opacity : 0 }, 200, function() {
+          graffiti.drawAreaStrokeCanvas.clearRect(0, 0, data[2], data[3]);
+          graffiti.drawAreaStrokeCanvas.style.opacity = "1";
+          graffiti.historyCheckpoint = undefined;
+        });
+      }
+      image.src = graffiti.historyCheckpoint;
+    } else {
+      graffiti.history.pop();
+      graffiti.drawAreaStrokeContext.drawImage(graffiti.drawAreaMainCanvas, 0, 0, data[0], data[1]);
+      graffiti.drawAreaMainContext.clearRect(0, 0, data[2], data[3]);
+      graffiti.draw(graffiti.drawAreaMainContext, graffiti.history);
+      animate(graffiti.drawAreaStrokeCanvas, { opacity : 0 }, 200, function() {
+        graffiti.drawAreaStrokeContext.clearRect(0, 0, data[2], data[3]);
+        graffiti.drawAreaStrokeCanvas.style.opacity = "1";
+      });
+    }
+  },
+
+  canvasCloneGetData: function() {
+    return [
+            parseInt(graffiti.drawAreaWrap.style.width),
+            parseInt(graffiti.drawAreaWrap.style.height),
+            graffiti.drawAreaMaxWidth,
+            graffiti.drawAreaMaxHeight
+           ];
+  }
 
 };
 
